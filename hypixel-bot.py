5fd873f3d485323bbcf95400b5d08b25324c94ce
Snuggle <snuggle@sprinkly.net>
2017-11-20 19:27:41 +0000

    Test

--- hypixel-bot.py
+++ hypixel-bot.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 __description__ = """ Unofficial Hypixel Discord Bot, Made by Snuggle. """
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 
 import discord
 from discord.ext import commands
