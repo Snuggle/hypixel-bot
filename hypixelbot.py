eb93b9e1b6f908f1290a2297bcb9653bd8a6443f
Snuggle <snuggle@sprinkly.net>
2018-02-06 19:53:00 +0000

    Bump version

--- hypixelbot.py
+++ hypixelbot.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 __description__ = """ Unofficial Hypixel Discord Bot, Made by Snuggle. """
-__version__ = '2018-02-04'
+__version__ = '2018-02-06'
 # Because this project isn't designed to be used by anyone, I feel like a date-
 # based version string is more useful than a conventional one.
 
